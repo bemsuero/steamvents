0210729c148d07bde9f9e7cefb74aeebc8df6d4e
Bemilton Suero <bemsuero@yahoo.com>
2018-10-01 12:46:25 -0400

    syntax error

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
     :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
-
+}
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
     :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
-
+}
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
